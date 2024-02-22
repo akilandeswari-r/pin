0e35f3b00afd8e218bd64976d31578cd5e28c78a
akilandeswari-r <160758135+akilandeswari-r@users.noreply.github.com>
2024-02-22 11:31:00 +0530

    Add files via upload

---
+++ pin.java
@@ -0,0 +1,21 @@
+public class pin {
+function calculateAmt(inquiry, withdrawal, deposit){
+                    var availableBal = 35000; 
+                    var a= inquiry(availableBal);
+                    var b= deposit(availableBal);
+                    var c= withdrawal(availableBal);
+                    if(a){
+
+                        availableBal =  inquiry(availableBal);
+                    }
+                    else if(b){
+                        availableBal =  availableBal - withdrawal;
+                        if(withdrawal > availableBal){
+                            alert("You have insufficient funds");  
+                        }
+                    }
+                    else{
+                        availableBal =  availableBal + deposit;
+                    }
+                    return availableBal;
+                }}
